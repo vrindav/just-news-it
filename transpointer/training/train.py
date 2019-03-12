e05fe320c120f4acd298b86a0768a901985133cb
vrindav <vrindav@stanford.edu>
2019-03-11 19:25:51 -0700

    And for the stars and stripes

--- transpointer/training/train.py
+++ transpointer/training/train.py
@@ -56,7 +56,7 @@ class Train(object):
         params = list(self.model.parameters())
         initial_lr = config.lr_coverage if config.is_coverage else config.lr
         self.optimizer = Adagrad(params, lr=initial_lr, initial_accumulator_value=config.adagrad_init_acc)
-        self.loss_func = torch.nn.CrossEntropyLoss(reduction='sum') #torch.nn.NLLLoss()
+        self.loss_func = torch.nn.CrossEntropyLoss(reduction='sum', ignore_index = 0)
 
         start_iter, start_loss = 0, 0
 
@@ -104,6 +104,7 @@ class Train(object):
         # padding is already done in previous function (see batcher.py - init_decoder_seq & init_decoder_seq - Batch class)
         self.optimizer.zero_grad()
         logits = self.model.forward(in_seq, in_pos, tgt_seq, tgt_pos, extra_zeros, enc_batch_extend_vocab)
+
 
         # compute loss from logits
         loss = self.loss_func(logits, tgt_seq[:, 1:].contiguous().view(-1))
