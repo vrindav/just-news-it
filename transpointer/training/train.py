290782367f6c2cf43d3e2a002a3f95cebae4769e
vrindav <vrindav@stanford.edu>
2019-03-14 17:28:39 -0700

    extractive

--- transpointer/training/train.py
+++ transpointer/training/train.py
@@ -120,9 +120,8 @@ class Train(object):
         for i in range(config.batch_size):
             target = target_batch[i]
             ex_logits = logits[i]
-            print(target)
-            print('\n')
-            print(ex_logits)
+            print(target.size())
+            print(ex_logits.size())
             target[ex_logits == 0] = 0
             losses.append(self.loss_func(ex_logits, target))
 
